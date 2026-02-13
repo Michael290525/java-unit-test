08a54a1394faa06853aa9153b170419f0977ca87
me <me@gmail.com>
2026-02-13 18:15:49 +0300

    test add

---
+++ task_3.java
@@ -0,0 +1,31 @@
+@RunWith(Parameterized.class)
+public class CheckIsAdultTest {
+	
+  private final int age;
+  private final boolean result;
+
+  public CheckIsAdultTest(int age, boolean result) {
+	this.result = result;
+    this.age = age;
+  }
+
+@Parameterized.Parameters
+  public static Object[][] getTextData() {
+	return new Object[][] {
+            {17, false},
+            {18, true},
+            {19, true},
+            {21, true},        
+		 // Заполни массив тестовыми данными и ожидаемым результатом
+    };
+  }
+
+  @Test
+  public void checkIsAdultWhenAgeThenResult() {
+	Program program = new Program();
+	 // Передай сюда возраст пользователя
+    boolean isAdult = program.checkIsAdult(age);
+	// Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
+    assertEquals("Должно вернуться: " + result + "для возраста " + age, result, isAdult);
+	}
+}
